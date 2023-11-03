04988278685e7bb8d2ede1dd026ba5dbc9b1d7cc
bryiac <bryceiancarter@gmail.com>
2023-11-03 01:12:03 -0400

    Moderation fixes

--- src/modules/Moderation.ts
+++ src/modules/Moderation.ts
@@ -19,7 +19,7 @@ type Events = {
   Unban: [Plr, Invoker, Reason],
 };
 
-type ModerationDatastore = Partial<{ [Key in "muted" | "banned" ]: { reason: Reason, expiry: number } }>;
+type ModerationDatastore = Partial<{ [Key in "muted" | "banned" ]: { invoker: number | undefined, reason: Reason, expiry: number } }>;
 
 export default class Moderation extends Module<Events> {
   private datastore: Datastore;
@@ -48,12 +48,14 @@ export default class Moderation extends Module<Events> {
 
   enable() {
     this.admin.messenger.on("message", (data) => {
-      const [eventType, action, plrIdId, invoker, reason, duration] = data as [EventType, keyof Events, Plr, Invoker, Reason, Duration];
+      const [eventType, action, plr, invoker, reason, duration] = data as [EventType, keyof Events, Plr, Invoker, Reason, Duration];
 
       if (eventType === EventType.Moderation && action && this[action]) {
         this.dispatch = false;
 
-        this[action](plrIdId, invoker, reason, duration);
+        this.logger.debug(`${eventType} ${action} ${plr} ${invoker} ${reason} ${duration}`);
+
+        this[action](plr, invoker, reason, duration);
       };
     });
 
@@ -75,14 +77,20 @@ export default class Moderation extends Module<Events> {
   };
 
   public Report(plr: Plr, invoker: Invoker, reason: Reason) {
+    this.logger.debug(`Action: Report, Player: ${plr}, Invoker ${invoker}, Reason: ${reason}`);
+
     this.dispatcher("Report", plr, invoker, reason);
   };
 
   public Warn(plr: Plr, invoker: Invoker, reason: Reason) {
+    this.logger.debug(`Action: Warn, Player: ${plr}, Invoker ${invoker}, Reason: ${reason}`);
+
     this.dispatcher("Warn", plr, invoker, reason);
   };
 
   public Kick(plr: Plr, invoker: Invoker, reason?: string) {
+    this.logger.debug(`Action: Kick, Player: ${plr}, Invoker ${invoker}, Reason: ${reason}`);
+
     const player = typeIs(plr, "number") ? Players.GetPlayerByUserId(plr) : plr;
 
     if (player) {
@@ -95,17 +103,22 @@ export default class Moderation extends Module<Events> {
   };
 
   public Mute(plr: Plr, invoker: Invoker, reason: Reason, duration: Duration) {
-    const id = tostring(plr);
+    this.logger.debug(`Action: Mute, Player: ${plr}, Invoker ${invoker}, Reason: ${reason}, Duration: ${duration}`);
+
+    const invokerId = typeIs(invoker, "number") ? invoker : invoker?.UserId;
+    const id = tostring(typeIs(plr, "number") ? plr : plr.UserId);
     const expiry = duration ? os.time() + duration : -1;
 
     this.datastore.update<ModerationDatastore>(id, (old) => {
-      return { ...old, muted: { reason, expiry } };
+      return { ...old, muted: { invoker: invokerId, reason, expiry } };
     });
 
     this.dispatcher("Mute", plr, invoker, reason, duration);
   };
 
   public Unmute(plr: Plr, invoker: Invoker, reason: Reason) {
+    this.logger.debug(`Action: Unmute, Player: ${plr}, Invoker ${invoker}, Reason: ${reason}`);
+
     const player = typeIs(plr, "number") ? Players.GetPlayerByUserId(plr) : plr;
     const id = tostring(plr);
 
@@ -125,17 +138,20 @@ export default class Moderation extends Module<Events> {
   };
 
   public Ban(plr: Plr, invoker: Invoker, reason: Reason, duration: Duration) {
+    this.logger.debug(`Action: Ban, Player: ${plr}, Invoker ${invoker}, Reason: ${reason}, Duration: ${duration}`);
+
     const id = tostring(typeIs(plr, "number") ? plr : plr.UserId);
+    const invokerId = typeIs(invoker, "number") ? invoker : invoker?.UserId;
     const expiry = duration ? os.time() + duration : -1;
 
     this.datastore.update<ModerationDatastore>(id, (old) => {
-      return { ...old, banned: { reason, expiry } };
+      return { ...old, banned: { invoker: invokerId, reason, expiry } };
     });
 
     const player = typeIs(plr, "number") ? Players.GetPlayerByUserId(plr) : plr;
 
     if (player) {
-      if (this.admin.config.moderation.kick) {
+      if (this.admin.config.moderation.ban) {
         player.Kick(reason);
       };
     }
@@ -144,6 +160,8 @@ export default class Moderation extends Module<Events> {
   };
 
   public Unban(plr: Plr, invoker: Invoker, reason: Reason) {
+    this.logger.debug(`Action: Unban, Player: ${plr}, Invoker ${invoker}, Reason: ${reason}`);
+
     const id = tostring(typeIs(plr, "number") ? plr : plr.UserId);
 
     this.datastore.update<ModerationDatastore>(id, (old) => {
@@ -162,19 +180,36 @@ export default class Moderation extends Module<Events> {
     const result = this.datastore.get<ModerationDatastore>(id);
 
     if (result) {
-      if (result.banned && result.banned.expiry > os.time() ) {
-        if (this.admin.config.)
-        plr.Kick(result.banned.reason);
-      } else {
-        this.Unban(plr.UserId, undefined, "Ban expired.");
+      if (result.banned) {
+        if (result.banned.expiry > os.time() || result.banned.expiry === -1) {
+          if (this.admin.config.moderation.ban) {
+            plr.Kick(result.banned.reason);
+          };
+
+          this.logger.debug(`Action: Ban Check, Player: ${plr}, Invoker ${result.banned.invoker}, Reason: ${result.banned.reason}, Expiry: ${result.banned.expiry}`);
+
+          this.emit("Ban", plr.UserId, result.banned.invoker, result.banned.reason, result.banned.expiry === -1 ? -1 : os.time() - result.banned.expiry);
+        } else {
+          this.dispatch = false;
+
+          this.Unban(plr.UserId, undefined, "Ban expired.");
+        };
       };
 
-      if (result.muted && result.muted.expiry > os.time()) {
-        if (result.muted.expiry > 0) {
-          this.mutes.set(plr.UserId, result.muted.expiry);
+      if (result.muted) {
+        if (result.muted.expiry > os.time() || result.muted.expiry === -1) {
+          if (result.muted.expiry > 0) {
+            this.mutes.set(plr.UserId, result.muted.expiry);
+          };
+
+          this.logger.debug(`Action: Mute Check, Player: ${plr}, Invoker ${result.muted.invoker}, Reason: ${result.muted.reason}, Expiry: ${result.muted.expiry}`);
+
+          this.emit("Mute", plr.UserId, result.muted.invoker, result.muted.reason, result.muted.expiry === -1 ? -1 : os.time() - result.muted.expiry);
+        } else {
+          this.dispatch = false;
+
+          this.Unmute(plr.UserId, undefined, "Mute expired.");
         };
-      } else {
-        this.Unmute(plr.UserId, undefined, "Mute expired.");
       };
     };
   };
@@ -199,19 +234,21 @@ export default class Moderation extends Module<Events> {
     const playerId = typeIs(plr, "number") ? plr : plr.UserId;
 
     if (this.dispatch) {
-      const gameId = game.GameId;
+      // const gameId = game.GameId;
 
-      const url = `${this.admin.config.api.base}/games/${gameId}/players/${playerId}/moderation`;
+      // const url = `${this.admin.config.api.base}/games/${gameId}/players/${playerId}/moderation`;
 
-      this.admin.messenger.post(url, {
-        action: action.lower(),
-        invoker,
-        reason,
-        duration
-      });
+      // this.admin.messenger.post(url, {
+      //   action: action.lower(),
+      //   invoker,
+      //   reason,
+      //   duration
+      // });
     } else {
       this.dispatch = true;
     };
+
+    this.logger.debug(`Dispatcher -> Action: ${action}, Player: ${plr}, Invoker ${invoker}, Reason: ${reason}, Duration: ${duration}`);
 
     this.emit(action, ...args);
   };
